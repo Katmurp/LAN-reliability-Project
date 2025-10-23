c5bafa0f6888e80fb401edc4bbe92d52a06d38e6
Katmurp <157540405+Katmurp@users.noreply.github.com>
2025-10-22 21:37:03 -0500

    Update runCustomCompoundNetworkSim.m

--- runCustomCompoundNetworkSim.m
+++ runCustomCompoundNetworkSim.m
@@ -1,10 +1,3 @@
-%% Function runSingleLinkSim() % Parameters 
-%  K - the number of packets in the application message 
-%  p - the probability of failure  
-%  N - the number of simulations to run 
-% 
-% Returns: the average numeric result across the total simulations 
- 
 function result = runCustomCompoundNetworkSim(K, p1, p2, p3, N)
 
     simResults = zeros(1, N);
@@ -15,17 +8,9 @@ function result = runCustomCompoundNetworkSim(K, p1, p2, p3, N)
             success = false;
             transmissions = 0;
 
-            % Each compound transmission continues until successful
+            % compound transmission continues until successful
             while ~success
                 transmissions = transmissions + 1;
-
-                % simulate the compound network structure:
-                % (two parallel links) in series with one link
-                %
-                % For simplicity:
-                % success = ( (parallel success between link1 & link2) ) AND (link3 success)
-                %
-                % Link1 and Link2 form parallel portion; Link3 is series.
 
                 link1_success = rand() > p1;
                 link2_success = rand() > p2;
@@ -33,7 +18,6 @@ function result = runCustomCompoundNetworkSim(K, p1, p2, p3, N)
 
                 link3_success = rand() > p3;
 
-                % Successful if both parallel portion and link3 succeed
                 success = parallel_success && link3_success;
             end
 
@@ -42,6 +26,6 @@ function result = runCustomCompoundNetworkSim(K, p1, p2, p3, N)
         simResults(i) = totalTransmissions;
     end
 
-    % Return the average number of transmissions across simulations
     result = mean(simResults);
+
 end
